8cca23b206c4c6f1079b5ea1c4598d2da2008451
SitDown Moody <siddhantmd@gmail.com>
2017-11-10 07:13:02 +0100

    Gaps and walls no longer coincide

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var endpt
 //var h = 800
 var lower_bnd = 0.5
 var upper_bnd = 1.5
+var array;
+
 function setup() {
     var w = windowHeight-120
     var h = windowHeight-120
@@ -20,70 +22,157 @@ function setup() {
     strokeWeight(gap/10)
     var circumf
     var num_gaps
-    console.log(radius)
-    for (iter = (radius/gap)-1; iter <= (w/2)/ gap -2; iter= iter+1){
-//    for (iter = 0; iter < (w/2)/ gap -2; iter= iter+1){
-        num_gaps = Math.floor(2.3 * Math.pow( ( 1+ (1/iter)) , iter ))
-        console.log("iter no. "+iter)
-        
-        circumf = 2 * PI * radius
-        var stpt = Math.floor (random (360))
-        
-        orig_stpt = stpt
-        gap_ang = ( gap / circumf) * 360
-        
-        for ( i = 0 ; i < num_gaps ; i++ ){
-            var stpt2 = Math.floor (random (360))
-            push()
-                translate(w/2, h/2)
-                stpt2= stpt2 + random (lower_bnd , upper_bnd) * (360/num_gaps)
-                rotate (stpt2)
-                if (iter < (w/2)/ gap -3){
-                line(radius,0,radius + gap,0);}
-            pop()
-        }
+    temp=(radius/gap)
+    
+    for (iter = temp; iter <= (w/2)/gap -1 ; iter= iter+2){
+        array=[];
 
-        for ( i = 0 ; i < num_gaps ; i++ ){
-//            var stpt2 = Math.floor (random (360))
-//            push()
-//                translate(w/2, h/2)
-//                stpt2= stpt2 + random (lower_bnd , upper_bnd) * (360/num_gaps)
-//                rotate (stpt2)
-//                if (iter < (w/2)/ gap -3){
-//                line(radius,0,radius + gap,0);}
-//            pop()
+        for (set_=0; set_<2; set_++){
+            array.push([])
+            temp3 = Math.floor(2.3 * Math.pow( ( 1+ (1/iter)) , iter ))
+            num_gaps =  Math.ceil (random (temp3))
+            console.log(num_gaps)
+            circumf = 2 * PI * radius
+            var stpt = Math.floor (random (360/num_gaps))
+
+            orig_stpt = stpt
+            gap_ang = ( gap / circumf) * 360            
+            console.log("org stpt "+orig_stpt)
+            console.log("num gaps "+ num_gaps)
+            console.log("gap ang "+ gap_ang)
+            console.log("")
             
-            endpt = stpt + random( gap_ang, random( lower_bnd, upper_bnd) * (360 / num_gaps) );
-            temp = endpt - Math.floor (endpt/360);
-            if (360-temp <  lower_bnd * (360-( num_gaps * gap_ang)) / num_gaps ){
-                    endpt = orig_stpt - gap_ang;
-                    push()
-                        translate (w/2, h/2);
-                        arc (0, 0, radius, radius, stpt, endpt ) 
-                    pop()                
-                    break;
-            }
-            if (i == num_gaps-1){
-                    endpt = orig_stpt - gap_ang;
-                    push()
-                        translate (w/2, h/2);
-                        arc (0, 0, radius, radius, stpt, endpt ) 
-                    pop()                
-                    break;
-            }
+            
+            for ( i = 0 ; i < num_gaps ; i++ ){
 
-            push()
-                translate (w/2, h/2);
-                arc (0, 0, radius, radius, stpt, endpt ) 
-            pop()
+                endpt = stpt + random( gap_ang, random( lower_bnd, upper_bnd) * (360 / num_gaps) );
+                
+                if ((endpt + gap_ang + ( upper_bnd * 360/num_gaps))-360 >  orig_stpt || i == num_gaps-1){
+                        endpt = orig_stpt - gap_ang+360;
+                        push()
+                            translate (w/2, h/2);
+                            arc (0, 0, radius, radius, stpt, endpt ) 
+                        pop()
+                    diff= endpt-stpt
+                    if (endpt > 360){
+                        if(set_==1){
+                            array[set_].push(stpt)
+                            array[set_].push(endpt)
+                        }
+                        endpt= endpt - 360
+                        stpt=endpt-diff
+                    }
+                    if (stpt > 360){
+                        if(set_==1){
+                            array[set_].push(stpt)
+                            array[set_].push(endpt)
+                        }
+                        stpt= stpt-360
+                        endpt=stpt+diff
+                    }
+                        array[set_].push(stpt)
+                        array[set_].push(endpt)
+                        break;
+                }
+                
+//                if (i == num_gaps-1){
+//                        if (endpt > orig_stpt + 360){
+//                            endpt = (orig_stpt - gap_ang + 360)
+//                        }
+////                        else {endpt = (orig_stpt - gap_ang) + 360};
+//                        push()
+//                            translate (w/2, h/2);
+//                            arc (0, 0, radius, radius, stpt, endpt ) 
+//                        pop() 
+//                        array[set_].push(stpt)
+//                        array[set_].push(endpt)
+//                        break;
+//                }
 
-            stpt = endpt + gap_ang
-        }
-    radius = radius + gap
+                push()
+                    translate (w/2, h/2);
+                    arc (0, 0, radius, radius, stpt, endpt ) 
+                pop()
+                diff= endpt-stpt
+                if (endpt > 360){
+                    if(set_==1){
+                            array[set_].push(stpt)
+                            array[set_].push(endpt)
+                        }
+                        endpt= endpt - 360
+                        stpt=endpt-diff
+                    }
+                    if (stpt > 360){
+                        if(set_==1){
+                            array[set_].push(stpt)
+                            array[set_].push(endpt)
+                        }
+                        
+                        stpt= stpt-360
+                        endpt=stpt+diff
+                    }
+                
+                array[set_].push(stpt)
+                array[set_].push(endpt)
+                stpt = endpt + gap_ang
+            }
+            
+        radius = radius + gap
+           
     }
+
+         for (strokes = 0; strokes < array[0].length ; strokes+=2){
+             
+            
+                temp2 = random( array [0] [strokes] , array [0] [strokes+1])
+                for (check=0; check < 100; check++){
+                    for (iu=0; iu <= array[0].length; iu+=2){
+    //                console.log(temp2 > array[1][iu] && temp2 < array[1][iu+1] )
+                                 console.log(temp2)
+
+                        if (temp2 > array[1][iu] && temp2 < array[1][iu+1]) {
+                            check = 112
+                            push()
+                                translate(w/2, h/2)
+                                rotate (temp2) 
+                                line(radius-2*gap,0,radius-gap,0);
+                            pop()
+
+                        }
+                    }
+                    temp2 = random( array [0] [strokes] , array [0] [strokes+1])
+                 }
+            
+             
+             
+
+                
+//                for ( check=0 ; check==0;){
+//                    if (temp2 > array[1][iu] && temp2 < array[1][iu] ){
+//                    push()
+//                        translate(w/2, h/2)
+//                        rotate (temp2) 
+//                        line(radius-2*gap,0,radius-gap,0);
+//                    pop()
+//                    check=1
+//                    } 
+//                    else{temp2=temp2+1}
+//                }
+
+                
+            
+        }
+            
+    }
+    
+
+console.log(array[0])
+console.log(array[1])    
 }
+
+
+
 
 function draw() {
     
 }
-
